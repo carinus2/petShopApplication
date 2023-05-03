f331705396dbc4be26a365712925752de7a6567b
carinus2 <carinanistor123321@gmail.com>
2023-05-03 17:36:03 +0300

    am facut pagina pt add pets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 //import java.sql.SQLException;
 
 
@@ -184,19 +182,13 @@ class MyFrame extends User implements ActionListener{
             String username = txtUsername.getText();
             String password = txtPassword.getText();
             if ((username.equals("Carina") || username.equals("Helena")) && password.equals("Zara")) {
-                try {
                     new MenuForShelter();
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
                 }
             }
 
         }
 
     }
-
-
-}
 
 class SelectUserFrame implements ActionListener {
     JFrame selectUserFrame = new JFrame("SelectUserFrame");
@@ -267,7 +259,7 @@ class MenuForShelter extends JFrame implements ActionListener{
     public JButton option4 = new JButton("Exit");
 
 
-    public MenuForShelter() throws SQLException {
+    public MenuForShelter() {
 
         setTitle("MenuForShelter");
         JPanel sidebar = new JPanel();
@@ -290,17 +282,20 @@ class MenuForShelter extends JFrame implements ActionListener{
         setSize(500, 500);
         setLocationRelativeTo(null);
         setVisible(true);
+        option1.addActionListener(this);
+        option2.addActionListener(this);
+        option3.addActionListener(this);
+        option4.addActionListener(this);
     }
 
-   
 
-    Connection connection = DriverManager.getConnection("jdbc:h2:mem:pets", "carinasihelena", "zara");
-
-   @Override
+    @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == option1) {
 
+        if(e.getSource() == option1) {
+            AddPet addPetFrame=new AddPet();
         }
+
         if (e.getSource() == option2) {
 
         }
@@ -315,8 +310,99 @@ class MenuForShelter extends JFrame implements ActionListener{
 
 }
 
+class AddPet {
+    JLabel nameField = new JLabel("Name:");
+    JTextField txtNameField = new JTextField();
+    JLabel breedField = new JLabel("Breed:");
+    JTextField txtBreedField = new JTextField();
+
+    JLabel ageField = new JLabel("Age:");
+    JTextField txtAgeField = new JTextField();
+
+    JLabel sexField = new JLabel("Sex:");
+    JTextField txtSexField = new JTextField();
+    JLabel weightField = new JLabel("Weight:");
+    JTextField txtWeightField = new JTextField();
+    JLabel temperamentField = new JLabel("Temperament:");
+    JTextField txtTemperamentField = new JTextField();
+
+    JPanel panel = new JPanel();
+
+    public void CenterFields() {
+
+    }
+
+
+
+    public AddPet() {
+        JFrame addPet = new JFrame("AddPetFrame");
+        addPet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addPet.setSize(500, 500);
+        addPet.getContentPane().setBackground(Color.pink);
+        addPet.setVisible(true);
+
+
+        int frameWidth = 500;
+        int frameHeight = 500;
+
+        int labelWidth = 70;
+        int fieldWidth = 200;
+
+
+        int labelHeight = 20;
+        int fieldHeight = 20;
+
+
+        int horizontalGap = 50;
+        int verticalGap = 20;
+
+
+
+        int labelX = (frameWidth - labelWidth - fieldWidth) / 2;
+        int fieldX = labelX + labelWidth + horizontalGap;
+        int nameY = (frameHeight - (7 * (labelHeight + verticalGap) + fieldHeight)) / 2;
+        int breedY = nameY + labelHeight + verticalGap;
+        int ageY = breedY + labelHeight + verticalGap;
+        int sexY = ageY + labelHeight + verticalGap;
+        int weightY = sexY + labelHeight + verticalGap;
+        int temperamentY = weightY + labelHeight + verticalGap;
+
+
+        nameField.setBounds(labelX, nameY, labelWidth, labelHeight);
+        txtNameField.setBounds(fieldX, nameY, fieldWidth, fieldHeight);
+        breedField.setBounds(labelX, breedY, labelWidth, labelHeight);
+        txtBreedField.setBounds(fieldX, breedY, fieldWidth, fieldHeight);
+        ageField.setBounds(labelX, ageY, labelWidth, labelHeight);
+        txtAgeField.setBounds(fieldX, ageY, fieldWidth, fieldHeight);
+        sexField.setBounds(labelX, sexY, labelWidth, labelHeight);
+        txtSexField.setBounds(fieldX, sexY, fieldWidth, fieldHeight);
+        weightField.setBounds(labelX, weightY, labelWidth, labelHeight);
+        txtWeightField.setBounds(fieldX, weightY, fieldWidth, fieldHeight);
+        temperamentField.setBounds(labelX, temperamentY, labelWidth, labelHeight);
+        txtTemperamentField.setBounds(fieldX, temperamentY, fieldWidth, fieldHeight);
+        addPet.getContentPane().add(nameField);
+        addPet.getContentPane().add(txtNameField);
+        addPet.getContentPane().add(breedField);
+        addPet.getContentPane().add(txtBreedField);
+        addPet.getContentPane().add(ageField);
+        addPet.getContentPane().add(txtAgeField);
+        addPet.getContentPane().add(sexField);
+        addPet.getContentPane().add(txtSexField);
+        addPet.getContentPane().add(weightField);
+        addPet.getContentPane().add(txtWeightField);
+        addPet.getContentPane().add(temperamentField);
+        addPet.getContentPane().add(txtTemperamentField);
+        addPet.setLayout(null);
+        addPet.setVisible(true);
+
+    }
+}
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "zara";
+        Connection conn = DriverManager.getConnection(url, username, password);
 
         new SelectUserFrame();
     }
